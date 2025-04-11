af000b23bcdd4233d720feea1559c552b1c8649e
ArmanHakhnazaryan <arman.hakhnazaryan@icloud.com>
2025-04-11 13:18:50 +0400

    forms test

--- news/forms.py
+++ news/forms.py
@@ -1,7 +1,7 @@
 from .models import Articles
 from django.forms import ModelForm, TextInput, DateTimeInput, Textarea
 
-
+#test git
 class ArticlesForm(ModelForm):
     class Meta:
         model = Articles
